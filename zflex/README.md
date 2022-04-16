408d68d1f80c4186c5a4722a8f25e37be4c3ba27
CrazyBolillo <antonio@zoftko.com>
2022-04-15 21:15:22 -0500

    [ADD] zflex: Responsive Odoo

---
+++ zflex/README.md
@@ -0,0 +1,6 @@
+# ZFlex
+This module makes Odoo's backend more responsive by adding features which use screen space much
+more effectively.
+
+## Features
+* Side chatter (as in Odoo Enterprise)
---
+++ zflex/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'ZoftKo Web',
+    'summary': 'Make Odoo responsive',
+    'version': '15.0.1.0',
+    'category': 'Website',
+    'website': 'https://github.com/ZoftKo',
+    'author': 'ZoftKo',
+    'installable': True,
+    'depends': ['web'],
+    'data': ['views/web.xml'],
+    'assets': {
+        'web.assets_backend': ['/zflex/static/src/scss/side_chatter.scss']
+    },
+    'license': 'GPL-3',
+}
