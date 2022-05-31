707576d75c813a8adbf76e7c5250ee60c1584fcf
CrazyBolillo <antonio@zoftko.com>
2022-05-30 19:08:16 -0500

    [ADD] zwarning: Warning on empty fields

---
+++ zwarning/static/src/js/WarningWidget.js
@@ -0,0 +1,32 @@
+/** @odoo-module **/
+
+import AbstractFieldOwl from 'web.AbstractFieldOwl';
+import FieldRegistry from 'web.field_registry_owl';
+
+const {useState} = owl.hooks;
+
+export class WarningWidget extends AbstractFieldOwl {
+    constructor(...args) {
+        super(...args);
+    }
+
+    setup() {
+        if (!('empty_val' in this.attrs)) {
+            throw new Error('You must specify empty_val to use widget="zwarning"')
+        }
+        this.state = useState({
+            isEmpty: this.value == this.attrs['empty_val'],
+            tooltip: this.attrs['empty_tooltip'] ?? ''
+        })
+    }
+}
+
+WarningWidget.template = owl.tags.xml`
+<div data-toggle="tooltip" data-placement="top" t-attf-title="{{ state.tooltip }}">
+    <span t-esc="_formatValue(value)"/>
+    <i t-if="state.isEmpty" class="fa fa-exclamation-triangle ml-2 text-danger"/>
+</div>
+`
+AbstractFieldOwl.supportedFieldTypes = ['float'];
+
+FieldRegistry.add('zwarning', WarningWidget);
