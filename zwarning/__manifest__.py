71b6fb89ea6b51d54bdf05e3a8b3e824fe032759
CrazyBolillo <antonio@zoftko.com>
2022-05-30 19:08:16 -0500

    [ADD] zwarning: Warning on empty fields

---
+++ zwarning/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'ZWarning',
+    'summary': 'Show warnings on empty fields',
+    'author': "ZoftKo",
+    'website': "https://github.com/ZoftKo",
+    'category': 'Technical',
+    'version': '15.0.1.0',
+    'depends': ['base'],
+    'data': [],
+    'assets': {
+        'web.assets_backend': ['zwarning/static/src/js/WarningWidget.js']
+    },
+    'demo': [],
+    'license': 'GPL-3'
+}
